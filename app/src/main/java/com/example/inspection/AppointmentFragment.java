1d6e2323ea4e8714a923fa1e579427b77ed09ec4
Sur.Vival <EvE>
2016-05-13 06:26:29 +0800

    some fix

--- app/src/main/java/com/example/inspection/AppointmentFragment.java
+++ app/src/main/java/com/example/inspection/AppointmentFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -32,6 +33,8 @@ public class AppointmentFragment extends Fragment {
     private RecyclerView rv;
     private CoordinatorLayout coordinatorLayout;
     private List<WebAppointment> webAppointments;
+    private SwipeRefreshLayout laySwip;
+    WebAppointmentDAO webAppDAO;
 
     public static final String EMP_ID = "empid";
 
@@ -46,19 +49,33 @@ public class AppointmentFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_recyclerview, container, false);
-        rv = (RecyclerView) view.findViewById(R.id.rv);
+        View view = inflater.inflate(R.layout.fragment_recyclerview_appointment, container, false);
+        rv = (RecyclerView) view.findViewById(R.id.rvAppointment);
+        laySwip = (SwipeRefreshLayout) view.findViewById(R.id.laySwipeAppointment);
 
         String empid = getArguments().getString(EMP_ID);
 
-        WebAppointmentDAO webAppDAO = new WebAppointmentDAO(getContext());
+        webAppDAO = new WebAppointmentDAO(getContext());
         webAppointments = webAppDAO.getAll();
         rv.setLayoutManager(new LinearLayoutManager(getContext()));
         rv.setAdapter(new AppointmentAdapter(getContext(), webAppointments));
 
+        init(view);
+
         return view;
     }
 
+    private void init(View view) {
+        laySwip.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                laySwip.setRefreshing(true);
+
+                webAppointments = webAppDAO.getAll();
+                rv.setAdapter(new AppointmentAdapter(getContext(), webAppointments));
+            }
+        });
+    }
 
 
 }
